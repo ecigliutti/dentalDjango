70014ea4ab50b7a6990442a55bd339a58d016229
Eduardo Cigliutti <eacigliutti@gmail.com>
2023-09-19 08:58:41 -0400

    initial commit

---
+++ website/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views #the period means from inside this directory (website)
+
+urlpatterns = [
+    
+    path('', views.home, name="home"), #the path '' means root path, views references views.py file
+    path('contact.html', views.contact, name="contact"), #the path 'not empty' means path to specific file, views references views.py file
+]
